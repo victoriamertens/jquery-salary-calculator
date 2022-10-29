9eed023092bc24d9d2d5f10d8642c6a9cb3b66d2
Victoria Mertens <victoria.mertens44@gmail.com>
2022-10-29 10:39:21 -0500

    addEmployee Function complete.

--- script.js
+++ script.js
@@ -2,4 +2,24 @@ $(document).ready(onReady);
 
 function onReady() {
   console.log('jquery and javascript ready');
+  $('#submit-btn').on('click', addEmployee);
+}
+
+let employeeArr = [];
+
+function addEmployee() {
+  console.log('in addEmployee');
+  let employee = {
+    firstname: $('#input-first-name').val(),
+    lastname: $('#input-last-name').val(),
+    ID: $('#input-ID').val(),
+    title: $('#input-title').val(),
+    annualSalary: $('#input-annual-salary').val(),
+  };
+  employeeArr.push(employee);
+  render();
+}
+
+function render() {
+  console.log('ready to render');
 }
